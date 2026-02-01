2eab213f71e8c760e354146490150794a0bdd9f6
VIKASG1304 <157728697+VIKASG1304@users.noreply.github.com>
2026-02-01 21:27:38 +0530

    python basic

---
+++ 04_practice.py
@@ -0,0 +1,4 @@
+a = input("enter the value of a ")
+b = input("enter the value of b ")
+print ("a is greater than b  : " , a > b)
+
---
+++ 06_practice.py
@@ -0,0 +1,4 @@
+a = float (input("enter the value of a "))
+
+
+print ("the square of a number is  " , a**2 )
